86aee9ceea53cfc5131664876e6f39d573a7d14b
George204 <97558179+George204@users.noreply.github.com>
2023-03-21 00:57:56 +0100

    Update 1.14.py

--- 1.14.py
+++ 1.14.py
@@ -1,7 +1,7 @@
 import random as r, msvcrt as m
 boki = (r.randrange(1,100)/10,r.randrange(1,100)/10,r.randrange(1,100)/10)
 if max(boki)>(sum(boki)-max(boki)):
-    print(boki, "\x1[20;0Htrójkąt nie możliwy")
+    print(boki, "\x1b[20;0Htrójkąt nie możliwy")
 else:
     print(boki, "\x1b[20;0Htrójkąt możliwy")
 m.getch()
