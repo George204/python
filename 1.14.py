5aab015e6f1405482cc530d3b3c72a5a0a6b0faf
George204 <97558179+George204@users.noreply.github.com>
2023-03-21 00:31:36 +0100

    Update 1.14.py

--- 1.14.py
+++ 1.14.py
@@ -1,5 +1,4 @@
-import random as r
-import msvcrt as m
+import random as r,msvcrt as m
 boki = (r.randrange(1,100)/10,r.randrange(1,100)/10,r.randrange(1,100)/10)
 print(boki,"\x1b[20;0H")
 if max(boki)>(sum(boki)-max(boki)):
